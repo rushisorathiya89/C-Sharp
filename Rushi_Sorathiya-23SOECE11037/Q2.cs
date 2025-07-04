1f9e1bf315b7e34959e9dafa232d902b309e8d2d
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-07-04 13:32:10 +0530

    cummit

---
+++ Rushi_Sorathiya-23SOECE11037/Q2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037
+{
+    internal class Q2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Q2");
+        }
+    }
+}
