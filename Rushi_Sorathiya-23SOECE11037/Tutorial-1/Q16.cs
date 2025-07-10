66197f9ecce01117eeea8a8802a480befcd0e579
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-07-10 21:27:43 +0530

    cummit

---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-1/Q16.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_1
+{
+    internal class Q16
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for (int i = 0; i < n; i++)
+            {
+                for(int j=1; j<=i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-1/Q17.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_1
+{
+    internal class Q17
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for(int i = 0; i < n; i++)
+            {
+                for (int j = 1; j <= i; j++) { 
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
