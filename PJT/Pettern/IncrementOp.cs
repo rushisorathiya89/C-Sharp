2cea8c0b2210e83e126bc4127d1a63c9f0b1cdeb
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-07-10 21:29:31 +0530

    cummit

---
+++ PJT/Pettern/IncrementOp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJT.Pettern
+{
+    internal class IncrementOp
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            //int b = 5; 0 
+            int res = a++;
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ PJT/Pettern/Tables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJT.Pettern
+{
+    internal class Tables
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1;  j <=10 ; j++)
+                {
+                    Console.WriteLine("{0} x {1} = {2}", i, j, i*j);
+                  
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
