f409ede71c3bd578384c3e7ccf1134f7214f95c5
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-07-11 20:40:03 +0530

    commit

---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-1/Q18.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_1
+{
+    internal class Q18
+    {
+        static void Main(string[] args)
+        {
+
+            int count = 1;
+            int n = 5;
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(count + " ");
+                    count++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
