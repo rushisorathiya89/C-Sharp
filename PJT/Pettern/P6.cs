ab7ab98cc735e33ebdcc44b342225b4295d06583
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-07-17 11:40:18 +0530

    complete

---
+++ PJT/Pettern/P6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJT.Pettern
+{
+    internal class P6
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            //int count = 1;
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (j == 1 && i>0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                    else
+                    {
+                        Console.Write(n+ " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ PJT/Pettern/P7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJT.Pettern
+{
+    internal class P7
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for (int i = 0; i <= n; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    if ((i+j) % 2 == 0)
+                    {
+                        Console.Write("# ");
+                    }
+                    else
+                    {
+                        Console.Write("* ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
