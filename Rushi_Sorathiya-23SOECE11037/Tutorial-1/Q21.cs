61e0d57354c0a2a341318d77da9cd7c829869c7b
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-07-19 23:06:20 +0530

    Q21

---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-1/Q21.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_1
+{
+    internal class Q21
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the number of terms:");
+            int term = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input number : ");
+            int n = Convert.ToInt32(Console.ReadLine()); ;
+            int sum = 0;
+            int temp = n;
+            for (int i = 0; i < term; i++)
+            {
+
+                if (i == term - 1)
+                {
+                    Console.Write(temp);
+                }
+                else
+                {
+                    Console.Write(temp + " + ");
+                }
+
+                sum += temp;
+                temp = n + (temp * 10);
+               
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sum of the series is: " + sum);
+        }
+    }
+}
