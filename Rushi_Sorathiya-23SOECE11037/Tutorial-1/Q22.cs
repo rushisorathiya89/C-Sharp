00361aa5dded037f54b6a45cc07b6d20c1de5a5c
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-07-22 20:21:41 +0530

    Q22

---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-1/Q22.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_1
+{
+    internal class Q22
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                for (int j = 1; j <= n - i; j++)
+                    Console.Write(" ");
+
+                for (int k = 1; k <= 2 * i - 1; k++)
+                    Console.Write("*");
+
+                Console.WriteLine();
+            }
+
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= n - i; j++)
+                    Console.Write(" ");
+
+                for (int k = 1; k <= 2 * i - 1; k++)
+                    Console.Write("*");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
