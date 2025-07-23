cc766a4898a54dcfff79542bec76012b1f06d447
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-07-23 22:24:47 +0530

    Q1

---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-2/Q1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_2
+{
+    internal class Q1
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+
+            Console.WriteLine("Elements in the array:");
+
+         
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
