9ed07ce2f4e8271a3ec3baaaef08e281b37accfa
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-07-29 11:50:40 +0530

    oop

---
+++ NB/OOP/Time.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NB.OOP
+{
+    class time
+    {
+        double hour;
+        double minute;
+        double second;
+        public void getData(double hour,double minute, double second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        public void showData()
+        {
+            Console.Write(hour +":");
+            Console.Write(minute + ":");
+            Console.Write(second + ":");
+        }
+
+    }
+    internal class Time
+    {
+        public static void Main(string[] args)
+        {
+            time t=new time();
+            t.getData(11,28,35);
+            t.showData();
+        }
+    }
+}
---
+++ NB/OOP/first.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NB.OOP
+{
+    public class Employee
+    {
+        int empId;
+        string eName;
+        double salary;
+        public void getData(int empId,string eName,double salary)
+        {
+            this.empId = empId;
+            this.eName = eName;
+            this.salary = salary;
+        }
+
+        public void showData()
+        {
+            Console.WriteLine("emp id: " + empId);
+            Console.WriteLine("emp name: " + eName);
+            Console.WriteLine("emp slary: " + salary);
+        }
+
+        public double getSalary()
+        {
+            return salary;
+        }
+    }
+    internal class first
+    {
+        public static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            e1.getData(1,"Rushi",150000);
+            //e1.showData();
+
+            Employee e2 = new Employee();
+            e2.getData(2,"Virus",12);
+            //e2.showData();
+
+            if(e1.getSalary()> e2.getSalary())
+            {
+                Console.WriteLine(""
+            }
+        }
+    }
+}
