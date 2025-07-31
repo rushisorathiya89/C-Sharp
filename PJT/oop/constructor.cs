cf8021b938a639b80caee2e09e9c0b3b3a1da927
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-07-31 19:48:39 +0530

    constructor overloading

---
+++ PJT/oop/constructor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJT.oop
+{
+    internal class constructor
+    {
+        class Employee
+        {
+            int empid;
+            string name;
+            string city;
+            public Employee()
+            {
+
+            }
+            public Employee(int empid,string name)
+            {
+                this.empid = empid;
+                this.name = name;
+            }
+            public Employee(int empid,string name,string city)
+            {
+                this.city = city;
+            }
+
+            public void Display()
+            {
+                Console.WriteLine("the id is :" +  empid);
+                Console.WriteLine("the name is : " + name);
+                Console.WriteLine("the city is : " + city);
+            }
+        }
+        static void Main(string[] args)
+        {
+          Employee emp = new Employee(1,"rushi");
+            emp.Display();
+
+        }
+    }
+}
