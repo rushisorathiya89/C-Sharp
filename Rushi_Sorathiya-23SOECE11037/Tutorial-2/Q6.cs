c36c51696ff9f0fada040901503b0b64806b801e
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-08-02 20:37:12 +0530

    Q6,7

---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-2/Q6.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_2
+{
+    internal class Q6
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            Console.WriteLine("Enter the element :");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int max = numbers[0];
+            int min = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+
+            Console.WriteLine("Maximum element: " + max);
+            Console.WriteLine("Minimum element: " + min);
+        }
+    }
+}
---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-2/Q7.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_2
+{
+    internal class Q7
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            Console.WriteLine("Enter the element :");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int[] even = new int[5];
+            int[] odd = new int[5];
+            for (int i = 0;i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    even[i] = numbers[i];
+                }
+                else
+                {
+                    odd[i] = numbers[i];
+                }
+            }
+            Console.WriteLine("Even numbers are:");
+            for (int i = 0; i < even.Length; i++)
+            {
+                if (even[i] != 0)
+                {
+                    Console.Write(even[i] + " ");
+                }
+            }
+            Console.WriteLine("\nOdd numbers are: ");
+            for (int i = 0; i < odd.Length; i++)
+            {
+                if (odd[i] != 0)
+                {
+                    Console.Write(odd[i] + " ");
+                }
+            }
+        }
+    }
+}
