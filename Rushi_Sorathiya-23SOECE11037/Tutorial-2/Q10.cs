5282240e34e7dc0df96820638cdc12903bb82156
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-08-03 23:26:55 +0530

    Q9,10

---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-2/Q10.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_2
+{
+    internal class Q10
+    {
+        static void Main(String[] args)
+        {
+
+            int[,] ary1 = new int[2, 2];
+            Console.WriteLine("Enter element of ary1 2x2 array: ");
+            for (int i = 0; i < ary1.GetLength(0); i++)
+            {
+                for (int j = 0; j < ary1.GetLength(0); j++)
+                {
+                    ary1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Matrix A:");
+
+            for (int i = 0; i < ary1.GetLength(0); i++)
+            {
+                for (int j = 0; j < ary1.GetLength(1); j++)
+                {
+                    Console.Write(ary1[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int[,] ary2 = new int[2, 2];
+            Console.WriteLine("Enter element of ary2 2x2 array: ");
+            for (int i = 0; i < ary2.GetLength(0); i++)
+            {
+                for (int j = 0; j < ary2.GetLength(0); j++)
+                {
+                    ary2[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Matrix B");
+
+            for (int i = 0; i < ary1.GetLength(0); i++)
+            {
+                for (int j = 0; j < ary1.GetLength(1); j++)
+                {
+                    Console.Write(ary1[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            int[,] sum = new int[2, 2];
+            for (int i = 0; i < ary1.GetLength(0); i++)
+            {
+                for (int j = 0; j < ary1.GetLength(1); j++)
+                {
+                    sum[i, j] = ary1[i, j] + ary2[i, j];
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Sum of Matrix A and B : ");
+            for (int i = 0; i < sum.GetLength(0); i++)
+            {
+                for (int j = 0; j < sum.GetLength(1); j++)
+                {
+                    Console.Write(sum[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-2/Q9.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_2
+{
+    internal class Q9
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            Console.WriteLine("Enter the element :");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter the position to remove element: ");
+            int pos=Convert.ToInt32(Console.ReadLine());
+            pos = pos - 1;
+            for (int i = pos;i < numbers.Length - 1;i++)
+            {
+                
+                 numbers[i] = numbers[i+1];
+                
+            }
+
+            Console.WriteLine("New Array");
+            for(int i = 0; i< numbers.Length-1;i++)
+            {
+               
+                 Console.Write(numbers[i] + " ");
+                
+            }
+        }
+    }
+}
