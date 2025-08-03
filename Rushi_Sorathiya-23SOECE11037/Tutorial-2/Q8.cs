be6bf8c744ffd914ff596e91e75576002c7175eb
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-08-03 22:03:38 +0530

    Q8

---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-2/Q8.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_2
+{
+    internal class Q8
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            Console.WriteLine("Enter the element :");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Array in desending order: ");
+            for(int i = 0;  i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] < numbers[j])
+                    {
+                        int temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
