30eafb2c01f76ac2c3897cbcd43fc201e63d6f9d
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-08-04 12:15:54 +0530

    Q1

---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-2.1/Q1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_2._1
+{
+    internal class Q1
+    {
+        static void Main(string[] args)
+        {
+            /* local variable definition */
+
+            int a = 10;
+
+
+
+            /* check the boolean condition using if statement */
+
+            if (a < 20)
+
+            {
+
+                /* if condition is true then print the following */
+
+                Console.WriteLine("a is less than 20");
+
+            }
+
+            Console.WriteLine("value of a is : {0}", a);
+
+            Console.ReadLine();
+        }
+    }
+}
