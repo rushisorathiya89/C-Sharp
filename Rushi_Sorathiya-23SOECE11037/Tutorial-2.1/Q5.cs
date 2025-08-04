beff43dcc9e1c2b6725366304e04dde741883bb9
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-08-04 12:16:43 +0530

    Q5

---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-2.1/Q5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_2._1
+{
+    internal class Q5
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];   
+            int sum = 0;              
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter Element {0}: ", i);
+                string str = Console.ReadLine();              
+                arr[i] = Convert.ToInt32(str);                
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                sum = sum + arr[i];       
+            }
+
+            Console.WriteLine("Sum of Elements : {0}", sum);
+            Console.ReadLine();           
+        }
+    }
+}
