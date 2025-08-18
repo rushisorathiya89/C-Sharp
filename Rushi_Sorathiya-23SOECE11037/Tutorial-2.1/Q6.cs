d9781d7d21b0968241930e9f8335661973bb295d
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-08-18 14:48:27 +0530

    Q6

--- Rushi_Sorathiya-23SOECE11037/Tutorial-2.1/Q6.cs
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-2.1/Q6.cs
@@ -19,14 +19,6 @@ namespace Rushi_Sorathiya_23SOECE11037.Tutorial_2._1
             Console.WriteLine(args[1]);              // ← Missing statement-2
             Console.WriteLine(args[2]);              // ← Missing statement-2
             Console.WriteLine(args[3]);              // ← Missing statement-2
-
-
-            // Missing statement-1 to 4:
-            //for (int i = 0; i < args.Length; i++)        // ← Missing statement-1
-            //{
-            //    Console.WriteLine(args[i]);              // ← Missing statement-2
-            //}                                           // ← Missing statement-3
-            //Console.ReadLine();                         // ← Missing statement-4
         }
     }
 }
