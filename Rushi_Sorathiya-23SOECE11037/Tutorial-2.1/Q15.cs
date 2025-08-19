02cde8e38c26abaae5425d2932502c92d24a8c2a
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-08-19 21:36:48 +0530

    Q15

---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-2.1/Q15.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_2._1
+{
+    internal class Q15
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine(); 
+            Console.WriteLine("You entered: " + name);  
+        }
+    }
+}
