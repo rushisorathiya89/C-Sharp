70234d8682e174b028e2a7e4b80ce177515c1fb8
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-08-20 18:25:25 +0530

    Q9

---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-2.1/Q9.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_2._1
+{
+    internal class Q9
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter the Value of N : ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int len = 2 * n + 2;
+            int[] arr = new int[len];
+            Console.WriteLine("Enter the Elements of Array : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("The Elements which occur exactly once. : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count == 1)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
+        }
+    }
+}
