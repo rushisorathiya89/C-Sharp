65188d25152e33531fb04489fa1f26e31f03645c
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-08-20 20:50:00 +0530

    Q11

---
+++ Rushi_Sorathiya-23SOECE11037/Tutorial-2.1/Q11.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rushi_Sorathiya_23SOECE11037.Tutorial_2._1
+{
+    internal class Q11
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("Enter number of elements: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+            int sum = 0;
+
+            Console.WriteLine("Enter the elements of array:");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                sum += arr[i]; 
+            }
+            Console.WriteLine("Sum of array elements = " + sum);
+        }
+    }
+}
