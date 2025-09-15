d09e9c5a0955133d1cd0de0bbdf1f79ec2cbb63d
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-09-15 12:12:20 +0530

    P9

---
+++ PJT/Pettern/P9.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJT.Pettern
+{
+ /*
+
+ * * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+        
+     */
+    internal class P9
+    {
+        static void Main(String[] args)
+        {
+            int n = 5;
+            for(int i=0; i<n; i++)
+            {
+                for(int j=0; j<i; j++)
+                {
+                    Console.Write("  ");
+                }
+                
+                for(int k=0; k< 2*n - ( (2*i) + 1 ); k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
