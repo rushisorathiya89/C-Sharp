fad8d686ac4bbeabdeebeb2d208b59f4840e7dcc
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-09-15 11:32:50 +0530

    P8

---
+++ PJT/Pettern/P8.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJT.Pettern
+{
+    internal class P8
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for(int i=0; i<n; i++)
+            {
+                for(int j=0; j<n-i-1; j++)
+                {
+                    Console.Write(" ");
+                }
+                //Console.WriteLine();
+
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
