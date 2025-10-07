0bc00f2125067baecaf426a43e73ebd152069017
rushisorathiya89 <rushisorathiya3@gmail.com>
2025-10-07 11:49:08 +0530

    MethodOverriding

---
+++ NB/OOP/MethodOverridingEx1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NB.OOP
+{
+    class Employee1
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public int Salary { get; set; }
+        public virtual double calbonus(double Salary)
+        {
+            return 20000.00;
+        }
+    }
+    class Manager1 : Employee1
+    {
+        public override double calbonus(double Salary)
+        {
+            double basesal = base.calbonus(Salary);
+            double calsal = Salary * 0.40;
+            if (basesal > calsal)
+            {
+                return basesal;
+            }
+            else
+            {
+                return calsal;
+            }
+        }
+    }
+    class Tester1 : Employee1
+    {
+        public override double calbonus(double Salary)
+        {
+            return 30000.00;
+        }
+    }
+    internal class MethodOverridingEx1
+    {
+        public static void Main(string[] args)
+        {
+            Employee1 emp = new Manager1
+            {
+                ID = 101,
+                Name = "naveen",
+                Salary = 50000,
+                Designation = "c# dev",
+            };
+            double bonus = emp.calbonus(emp.Salary);
+            Console.WriteLine($"Emp details: {emp.ID},{emp.Name},{emp.Salary},{emp.Designation}");
+
+            Employee1 emp2 = new Manager1
+            {
+                ID = 215,
+                Name = "Rushi",
+                Salary = 50000,
+                Designation = "c# dev",
+            };
+            bonus = emp2.calbonus(emp2.Salary);
+            Console.WriteLine($"Emp 2 details: {emp2.ID},{emp2.Name},{emp2.Salary},{emp2.Designation}");
+        }
+    }
+}
+
---
+++ NB/OOP/Methodoverrinding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NB.OOP
+{
+    class P
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("Parent class");
+        }
+    }
+    class C : P
+    {
+        public override void show()
+        {
+            P obj=new P();
+            obj.show();
+            Console.WriteLine("Child class");
+        }
+    }
+    internal class Methodoverrinding
+    {
+        static void Main(string[] args)
+        {
+            P pobj;
+            C cobj = new C();
+            pobj = cobj;
+            pobj.show();
+            cobj.show();
+        }
+    }
+}
